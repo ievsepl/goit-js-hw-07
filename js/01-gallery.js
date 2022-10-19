894f0a46ea0dbdcdd93723b44654d1de8239c83d
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-10-19 17:15:56 +0300

    add my work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-
 const newGallery = onCreateNewItem(galleryItems);
 
 function onCreateNewItem(galleryItems) {
@@ -11,12 +10,12 @@ function onCreateNewItem(galleryItems) {
       ({ preview, description, original }) =>
         `<div class="gallery__item">
     
-  <a class="gallery__link"   href="large-image.jpg">
+  <a class="gallery__link"   href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
        alt="${description}"
-      data-source=${original}
+      data-source="${original}"
     />
   </a>
 </div>`
@@ -26,29 +25,25 @@ function onCreateNewItem(galleryItems) {
 gallery.insertAdjacentHTML("afterbegin", newGallery);
 
 gallery.addEventListener("click", onOpenPic);
-
 function onOpenPic(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
   const largeImage = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
   largeImage.show();
-  window.addEventListener("keydown", onEscapeClose);
+  gallery.addEventListener("keydown", onEscapeClose);
+
   function onEscapeClose(e) {
     console.log(e.code);
     if (e.code === "Escape") {
       largeImage.close();
     }
   }
-  window.removeEventListener("keydown", onEscape);
-  function onEscape(e) {
-    console.log(e.code);
-    if (largeImage.close()) {
-      return;
+  gallery.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      gallery.removeEventListener("keydown", onEscapeClose);
     }
-  }
+  });
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const newGallery = onCreateNewItem(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", newGallery);
+
+gallery.addEventListener("click", onOpenPic);
+
+const largeImage = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+function onCreateNewItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        `<a class="gallery__item"   href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+       alt="${description}"      
+    />
+  </a>`
+    )
+    .join("");
+}
+function onOpenPic(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  largeImage.open(event.target);
+}
